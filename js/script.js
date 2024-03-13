c2a50919c1d0dbb6343d376243308c6fadf26692
Your Name <you@example.com>
2024-03-13 07:25:40 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+      autoplay:{
+          delay: 3000,
+          disableOnInteraction: false,
+      },
+  
+    pagination: {
+        clickable: true,
+      el: '.swiper-pagination',   
+    },
+  
+   
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  
+  });
+  
+  const bars = document.querySelector('.fa-bars'),
+    navigation = document.querySelector('.navigation');
+  
+  
+    bars.addEventListener('click', () =>{
+      if(navigation.classList.contains('long')){
+        navigation.classList.add('short');
+        navigation.classList.remove('long');
+  
+      }
+      else{
+        navigation.classList.add('long');
+        navigation.classList.remove('short');
+      }
+    })
